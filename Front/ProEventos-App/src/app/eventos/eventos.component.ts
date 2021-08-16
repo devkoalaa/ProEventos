d3a0567d6a1fb82efa3bd5dc58dba74ed7ed64ef
herbert.valadares <herberticleber@hotmail.com>
2021-08-16 16:47:26 -0300

    Subindo alterações.

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,11 +6,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './eventos.component.html',
   styleUrls: ['./eventos.component.scss']
 })
+
 export class EventosComponent implements OnInit {
 
-  constructor() { }
+  public eventos: any;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getEventos();
   }
 
+  public getEventos(): void {
+    this.http.get('https://localhost:5001/api/eventos').subscribe(
+      response => this.eventos = response,
+      error => console.log('errorrrrr: ', error)
+    );
+  }
 }
